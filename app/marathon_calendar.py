58efee5204e32f4e6b4b2344ae700660edb7addd
arthur <atharp@uci.edu>
2021-12-06 16:12:04 -0800

    added route for getting the week workout, hard coded values for now

--- app/marathon_calendar.py
+++ app/marathon_calendar.py
@@ -8,6 +8,8 @@ from flask import request
 from flask import url_for
 from werkzeug.exceptions import abort
 from app.db import get_all_runs
+from app.utils import get_week_workout_as_dict
+import app.models as Models
 
 marathon_calendar = Blueprint("marathon_calendar", __name__)
 
@@ -20,3 +22,11 @@ def homepage():
         runs = get_all_runs(mileage)
         return render_template('marathon_calendar/index.html', runs=runs, mileage=mileage)
     return render_template('home.html')
+
+@marathon_calendar.route("/get_workout", methods=["GET"])
+def get_workout():
+    # workoutForWeek = get_workout_for_week(20, quality_1, quality_two)
+    weekWorkOutWithQualitySessions = Models.WeekWorkOutWithQualitySessions(40, 13, 11)#Models.WeekWorkout(40)
+    runs = weekWorkOutWithQualitySessions.get_workout_for_week()
+    runs_for_week = get_week_workout_as_dict(runs)
+    return render_template('marathon_workout_week/index.html', runs_for_week=runs_for_week)
